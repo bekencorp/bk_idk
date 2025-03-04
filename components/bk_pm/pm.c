f2de2e66b88862a8ad7c5fb7bb9c69c1e1f42baa
itester <itester@aclsemi.com>
2025-03-04 10:14:10 +0800

    [CI] [52886]  <pm> <perf>Using 0x1LLU for 64bit low voltage vote when setting value

--- components/bk_pm/pm.c
+++ components/bk_pm/pm.c
@@ -587,7 +587,7 @@ bk_err_t bk_pm_module_vote_power_ctrl(pm_power_module_name_e module, pm_power_mo
 			s_pm_audio_pm_state |= 0x1 << (module % (PM_POWER_MODULE_NAME_AUDP * PM_MODULE_SUB_POWER_DOMAIN_MAX));
 			s_pm_off_modules &= ~(0x1 << PM_POWER_MODULE_NAME_AUDP);
 			s_pm_on_modules |= 0x1 << PM_POWER_MODULE_NAME_AUDP;
-			s_pm_sleeped_modules &= ~(0x1 << PM_POWER_MODULE_NAME_AUDP);
+			s_pm_sleeped_modules &= ~(0x1ULL << PM_POWER_MODULE_NAME_AUDP);
 			GLOBAL_INT_RESTORE();
 
 			if (s_pm_cp1_auto_power_down_flag)
@@ -621,7 +621,7 @@ bk_err_t bk_pm_module_vote_power_ctrl(pm_power_module_name_e module, pm_power_mo
 			s_pm_video_pm_state |= 0x1 << (module % (PM_POWER_MODULE_NAME_VIDP * PM_MODULE_SUB_POWER_DOMAIN_MAX));
 			s_pm_off_modules &= ~(0x1 << PM_POWER_MODULE_NAME_VIDP);
 			s_pm_on_modules |= 0x1 << PM_POWER_MODULE_NAME_VIDP;
-			s_pm_sleeped_modules &= ~(0x1 << PM_POWER_MODULE_NAME_VIDP);
+			s_pm_sleeped_modules &= ~(0x1ULL << PM_POWER_MODULE_NAME_VIDP);
 			GLOBAL_INT_RESTORE();
 
 			if (s_pm_cp1_auto_power_down_flag)
@@ -704,7 +704,7 @@ bk_err_t bk_pm_module_vote_power_ctrl(pm_power_module_name_e module, pm_power_mo
 			GLOBAL_INT_DISABLE();
 			s_pm_off_modules &= ~(0x1 << PM_POWER_MODULE_NAME_BTSP);
 			s_pm_on_modules |= 0x1 << PM_POWER_MODULE_NAME_BTSP;
-			s_pm_sleeped_modules &= ~(0x1 << PM_POWER_MODULE_NAME_BTSP);
+			s_pm_sleeped_modules &= ~(0x1ULL << PM_POWER_MODULE_NAME_BTSP);
 			GLOBAL_INT_RESTORE();
 		}
 		else
@@ -785,7 +785,7 @@ bk_err_t bk_pm_module_vote_power_ctrl(pm_power_module_name_e module, pm_power_mo
 				sys_drv_module_power_ctrl(PM_POWER_MODULE_NAME_AUDP, power_state);
 				s_pm_off_modules |= 0x1 << PM_POWER_MODULE_NAME_AUDP;
 				s_pm_on_modules &= ~(0x1 << PM_POWER_MODULE_NAME_AUDP);
-				s_pm_sleeped_modules |= (0x1 << PM_POWER_MODULE_NAME_AUDP);
+				s_pm_sleeped_modules |= (0x1ULL << PM_POWER_MODULE_NAME_AUDP);
 			}
 			GLOBAL_INT_RESTORE();
 		}
@@ -807,7 +807,7 @@ bk_err_t bk_pm_module_vote_power_ctrl(pm_power_module_name_e module, pm_power_mo
 				sys_drv_module_power_ctrl(PM_POWER_MODULE_NAME_VIDP, power_state);
 				s_pm_off_modules |= 0x1 << PM_POWER_MODULE_NAME_VIDP;
 				s_pm_on_modules &= ~(0x1 << PM_POWER_MODULE_NAME_VIDP);
-				s_pm_sleeped_modules |= (0x1 << PM_POWER_MODULE_NAME_VIDP);
+				s_pm_sleeped_modules |= (0x1ULL << PM_POWER_MODULE_NAME_VIDP);
 			}
 			GLOBAL_INT_RESTORE();
 		}
@@ -1036,7 +1036,7 @@ bk_err_t bk_pm_module_vote_sleep_ctrl(pm_sleep_module_name_e module, uint32_t sl
 		else if (module == PM_SLEEP_MODULE_NAME_APP)
 		{
 		}
-		s_pm_sleeped_modules &= ~(0x1 << module);
+		s_pm_sleeped_modules &= ~(0x1ULL << module);
 	}
 	if (s_debug_en & 0x1)
 		os_printf("pm sleep state 0x%llX 0x%x %d\r\n", s_pm_sleeped_modules, s_pm_on_modules, module);
@@ -1184,28 +1184,28 @@ static void pm_check_power_on_module()
 
 	if (!(s_pm_on_modules & ((0x1 << PM_POWER_MODULE_NAME_BTSP)))) // when the module not power on , set the module sleep state
 	{
-		s_pm_sleeped_modules |= 0x1 << PM_POWER_MODULE_NAME_BTSP;
+		s_pm_sleeped_modules |= 0x1ULL << PM_POWER_MODULE_NAME_BTSP;
 		s_pm_off_modules |= 0x1 << PM_POWER_MODULE_NAME_BTSP;
 		// os_printf("bt not power on \r\n");
 	}
 
 	if (!(s_pm_on_modules & (0x1 << PM_POWER_MODULE_NAME_WIFIP_MAC))) // when the module not power on , set the module sleep state
 	{
-		s_pm_sleeped_modules |= 0x1 << PM_POWER_MODULE_NAME_WIFIP_MAC;
+		s_pm_sleeped_modules |= 0x1ULL << PM_POWER_MODULE_NAME_WIFIP_MAC;
 		s_pm_off_modules |= 0x1 << PM_POWER_MODULE_NAME_WIFIP_MAC;
 		// os_printf("wifi not power on \r\n");
 	}
 
 	if (!(s_pm_on_modules & (0x1 << PM_POWER_MODULE_NAME_AUDP))) // when the module not power on , set the module sleep state
 	{
-		s_pm_sleeped_modules |= 0x1 << PM_POWER_MODULE_NAME_AUDP;
+		s_pm_sleeped_modules |= 0x1ULL << PM_POWER_MODULE_NAME_AUDP;
 		s_pm_off_modules |= 0x1 << PM_POWER_MODULE_NAME_AUDP;
 		// os_printf("audio not power on \r\n");
 	}
 
 	if (!(s_pm_on_modules & (0x1 << PM_POWER_MODULE_NAME_VIDP))) // when the module not power on , set the module sleep state
 	{
-		s_pm_sleeped_modules |= 0x1 << PM_POWER_MODULE_NAME_VIDP;
+		s_pm_sleeped_modules |= 0x1ULL << PM_POWER_MODULE_NAME_VIDP;
 		s_pm_off_modules |= 0x1 << PM_POWER_MODULE_NAME_VIDP;
 		// os_printf("video not power on \r\n");
 	}
