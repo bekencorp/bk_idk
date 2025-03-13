9a94d16af67aa9648a67bee005f1de0c062ccc8e
itester <itester@aclsemi.com>
2025-03-13 20:01:46 +0800

    [CI] [53757,53758]  <multimedia> <fix> Project doorbell_ab_4M cannot run.

--- tools/build_tools/part_table_tools/otherScript/special_project_deal.py
+++ tools/build_tools/part_table_tools/otherScript/special_project_deal.py
@@ -252,7 +252,7 @@ def main():
         "bk7256_configa": project_bk7256_configa_deal,
         "bk7256_configb": project_bk7256_configb_deal,
         "config_ab": project_config_ab_deal,
-        "doorbell_cs2_ab_4M": project_config_ab_deal,
+        "doorbell_ab_4M": project_config_ab_deal,
     }
 
     parser = argparse.ArgumentParser(description='Deal with special json files from special project')
