8818367c98b4fc3de0e8931ac9b57eaf2207a95b
itester <itester@aclsemi.com>
2025-03-28 18:03:45 +0800

    [CI] [55068]  <pm> <perf>when xQueueGenericCreate first use psram, not need send msg to cpu0,because cpu0 have inited the psram

--- components/os_source/freertos_v10/portable/MemMang/heap_4.c
+++ components/os_source/freertos_v10/portable/MemMang/heap_4.c
@@ -573,7 +573,12 @@ void *psram_malloc( size_t xWantedSize )
 		/* 4 Byte alignment required for psram. */
 		xWantedSize += ( 0x4 - ( xWantedSize & 0x3 ) );
 	}
-
+    #if CONFIG_SYS_CPU0
+	if( psram_pxEnd == NULL || !bk_psram_heap_init_flag_get())
+	{
+		bk_pm_module_vote_psram_ctrl(PM_POWER_PSRAM_MODULE_NAME_AS_MEM,PM_POWER_MODULE_STATE_ON);
+	}
+    #endif
 	vTaskSuspendAll();
 	pvReturn = psram_malloc_without_lock(xWantedSize  + MEM_CHECK_TAG_LEN);
 	if(pvReturn)
