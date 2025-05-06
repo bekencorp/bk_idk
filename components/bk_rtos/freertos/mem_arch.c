6716b4c12bc6f3853c9e741831170ecea0a7ba36
itester <itester@aclsemi.com>
2025-05-06 19:30:54 +0800

    [CI] [57382]  <platform> <feat> add a new interface

--- components/bk_rtos/freertos/mem_arch.c
+++ components/bk_rtos/freertos/mem_arch.c
@@ -77,6 +77,28 @@ void *bk_psram_realloc(void *ptr, size_t size)
 	return tmp;
 }
 
+void *psram_realloc(void *ptr, size_t size)
+{
+	void *tmp;
+
+	if (platform_is_in_interrupt_context() && (arch_is_enter_exception() == 0)) {
+		os_printf("psram_realloc_risk\r\n");
+		BK_ASSERT(false);
+	}
+
+	tmp = psram_malloc(size);
+	if (tmp && ptr) {
+		if (size & 0x3) {
+			os_memcpy_word((uint32_t *)tmp, (uint32_t *)ptr, ((size >> 2) + 1) << 2);
+		} else {
+			os_memcpy_word((uint32_t *)tmp, (uint32_t *)ptr, size);
+		}
+		os_free((void *)ptr);
+	}
+
+	return tmp;
+}
+
 int os_memcmp_const(const void *a, const void *b, size_t len)
 {
 	return memcmp(a, b, len);
