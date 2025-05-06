076c510183c0754556a445dd34a7435edaaaea16
itester <itester@aclsemi.com>
2025-05-06 19:30:51 +0800

    [CI] [57403]  <platform> <feat>  add a new interface

--- include/os/mem.h
+++ include/os/mem.h
@@ -139,6 +139,7 @@ void *os_realloc(void *ptr, size_t size);
   * 
   */
 void *bk_psram_realloc(void *ptr, size_t size);
+void *psram_realloc(void *ptr, size_t size);
 
 
 #if (CONFIG_MALLOC_STATIS || CONFIG_MEM_DEBUG)
